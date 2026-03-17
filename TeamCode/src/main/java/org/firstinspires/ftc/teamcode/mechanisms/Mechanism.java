c900f9714a7029f44743c72606cb58dc893021b2
Roy Brabson <brabson@alumni.duke.edu>
2026-03-16 23:22:17 -0400

    code cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Mechanism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Mechanism.java
@@ -1,5 +1,13 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+/**
+ * An interface for mechanisms. A mechanism is a subsystem of the robot that performs a specific
+ * function, such as driving, shooting, or intaking. The update method is called every loop to
+ * update the mechanism's state.
+ */
 public interface Mechanism {
+    /**
+     * Updates the mechanism's state. This method is called every loop to update the mechanism's state.
+     */
     void update();
 }
